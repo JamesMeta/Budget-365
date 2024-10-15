fdfb74ed7d93effe500d49776afd2bd27b048f50
JamesMeta <jamesmata556@gmail.com>
2024-10-14 22:37:00 -0400

    began working on the homepage, is going to require investigating a better approach for the center table, since we are going to wanna differentiate between the day by a divider. Will most likely require us to design our own widget

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -13,7 +13,7 @@ class Budget365 extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
       home: const Budget365Widget(title: 'Budget365'),
@@ -31,11 +31,183 @@ class Budget365Widget extends StatefulWidget {
 }
 
 class _Budget365WidgetState extends State<Budget365Widget> {
+  void _goToSettings() {
+    // Navigator.push(
+    //   context,
+    //   MaterialPageRoute(builder: (context) => const SettingsPage()),
+    // );
+  }
+
+  void _goToCalendar() {
+    // Navigator.push(
+    //   context,
+    //   MaterialPageRoute(builder: (context) => const CalendarPage()),
+    // );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          leading: Image.asset('assets/images/logo.png'),
+          title:
+              Text(widget.title, style: const TextStyle(color: Colors.white)),
+          actions: [
+            IconButton(
+                onPressed: _goToSettings,
+                icon: const Icon(
+                  Icons.settings,
+                  color: Colors.white,
+                  size: 30,
+                )),
+          ],
+          leadingWidth: 100,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const DropdownMenuGroup(),
+                  IconButton(
+                      onPressed: _goToCalendar,
+                      icon: const Icon(Icons.calendar_month,
+                          color: Colors.white, size: 30)),
+                ],
+              ),
+              const SizedBox(height: 20),
+              ListView(
+                children: const [
+                  ListTile(
+                    title: Text('Item 1'),
+                  ),
+                  ListTile(
+                    title: Text('Item 2'),
+                  ),
+                  ListTile(
+                    title: Text('Item 3'),
+                  ),
+                  ListTile(
+                    title: Text('Item 4'),
+                  ),
+                  ListTile(
+                    title: Text('Item 5'),
+                  ),
+                  ListTile(
+                    title: Text('Item 6'),
+                  ),
+                  ListTile(
+                    title: Text('Item 7'),
+                  ),
+                  ListTile(
+                    title: Text('Item 8'),
+                  ),
+                  ListTile(
+                    title: Text('Item 9'),
+                  ),
+                  ListTile(
+                    title: Text('Item 10'),
+                  ),
+                  ListTile(
+                    title: Text('Item 11'),
+                  ),
+                  ListTile(
+                    title: Text('Item 12'),
+                  ),
+                  ListTile(
+                    title: Text('Item 13'),
+                  ),
+                  ListTile(
+                    title: Text('Item 14'),
+                  ),
+                  ListTile(
+                    title: Text('Item 15'),
+                  ),
+                  ListTile(
+                    title: Text('Item 16'),
+                  ),
+                  ListTile(
+                    title: Text('Item 17'),
+                  ),
+                  ListTile(
+                    title: Text('Item 18'),
+                  ),
+                  ListTile(
+                    title: Text('Item 19'),
+                  ),
+                  ListTile(
+                    title: Text('Item 20'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: const Color.fromARGB(255, 43, 118, 179),
+          selectedItemColor: Colors.grey,
+          unselectedItemColor: Colors.white,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          iconSize: 40,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bar_chart),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.group),
+              label: '',
+            ),
+          ],
+        ));
+  }
+}
+
+class DropdownMenuGroup extends StatefulWidget {
+  const DropdownMenuGroup({super.key});
+
+  @override
+  State<DropdownMenuGroup> createState() => _DropdownMenuGroupState();
+}
+
+class _DropdownMenuGroupState extends State<DropdownMenuGroup> {
+  String? _selectedItem = 'Group1';
+
+  List<String> _items = <String>['Group1', 'Group2', 'Group3'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: 200,
+        child: DropdownButton<String>(
+          style: const TextStyle(color: Colors.white),
+          dropdownColor: Colors.blue,
+          menuWidth: 200,
+          isExpanded: true,
+          value: _selectedItem,
+          onChanged: (String? value) {
+            setState(() {
+              _selectedItem = value;
+            });
+          },
+          items: _items.map((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
